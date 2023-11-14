5ebcfc7bd255fedec15c85b7c86d3b86b37c6e6d
raissape <86750496+raissape@users.noreply.github.com>
2023-11-14 20:17:38 -0300

    Add files via upload

---
+++ BalanceadorUm.java
@@ -0,0 +1,32 @@
+package SisDis;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.Writer;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+public class BalanceadorUm {
+	
+	private static int serv1Quantity = 0;
+	private static int serv2Quantity = 0;
+	
+	
+
+}
