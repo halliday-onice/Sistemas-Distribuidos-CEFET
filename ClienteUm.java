a62724cdaaecbfb0a1211433b3eabc6aef0a6ab3
halliday-onice <jonatashalliday1206@gmail.com>
2023-11-13 23:59:37 -0300

    Ajustes feitos- falta conseguir testar- dando erro linha 15 client

--- ClienteUm.java
+++ ClienteUm.java
@@ -5,14 +5,17 @@ import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Random;
-import java.util.Math;
+//import java.util.Math;
 
 public class ClienteUm implements Runnable{
-      ServidorUm portAccess = ServidorUm.serverPort;
+      
       private PrintStream out;
-      public static void main(String[] arg) throws UnknownHostException,IOException, InterruptedException{
-            Socket accept = new Socket(portAccess);
-            PrintStream output = new PrintStream(accept.getOutputStream());
+
+      public static void main(String[] arg) throws UnknownHostException, IOException, InterruptedException{
+            int portAccess = ServidorUm.serverPort;
+            System.out.println("A porta do servidor eh " + portAccess);
+            Socket sock = new Socket();
+            PrintStream output = new PrintStream(sock.getOutputStream());
             System.out.println("Output Stream: " + output);
 
             while(true) {
@@ -25,6 +28,11 @@ public class ClienteUm implements Runnable{
             }
       }
 
+      // preciso inicializar o constructor para poder enviar os dados ao servidor
+      public ClienteUm(PrintStream out){
+            this.out = out;
+      }
+
       public void run(){
             //eh nessa funcao run onde ocorrera o processamento da thread
             // Dado que a restricao eh: 
@@ -33,11 +41,24 @@ public class ClienteUm implements Runnable{
             //verificação do MDC (Máximo Divisor Comum) entre esses 2 números.
             //static int sleepDuration = (int) (Math.random() * (200 - 50 + 1) + 50);//calcula os numeros entre 50 e 200, inclusive add 50 pra deslocar todo o intervalo pra que comece em 50
             // thread tem que dormir por um tempo especificado entre 50 e 200 ms
-            Random rndOne = new Random();
-            Random rndTwo = new Random();
-            int randomOne = rndOne.nextInt(99999) + 2;
-            int randomTwo = rndTwo.nextInt(99999) + 2;
+            try {
+                  Random rndOne = new Random();
+                  Random rndTwo = new Random();
+                  int randomOne = rndOne.nextInt(99999) + 2;
+                  int randomTwo = rndTwo.nextInt(99999) + 2;
+
+                  // envia os numeros para o servidor
+                  out.println(randomOne);
+                  out.println(randomTwo);
+
+                  Thread.sleep(randomOne);
+                  Thread.sleep(randomTwo);
+
+            } catch(InterruptedException e){
+                  e.printStackTrace();
+            }
             
+
       }
 
 }
--- ServidorUm.java
+++ ServidorUm.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
@@ -24,21 +25,28 @@ public class ServidorUm implements Runnable{ // Any class that implements the ru
       public static int counterR = 0; // counter read files
 
       Semaphore semaphore = new Semaphore(counterR);
+
+      public static int serverPort = 4000;
+
+      
       public static void main(String[] arg) throws InterruptedException,IOException
       {
-            final int serverPort = 4000;
+            
             
             // tem algumas coisas a mais aqui nesse meio do caminho...
 
 
-            try{
-                  ServerSocket serverSocket = new  ServerSocket(serverPort);
+            try(ServerSocket serverSocket = new  ServerSocket(serverPort)){
+            
                   System.out.println("O Servidor esta ouvindo na porta " + serverPort);
 
                   
                   //Semaphore semaphore = new Semaphore(serverPort);
                   while(true)
                   {
+                        Socket clientSocket = serverSocket.accept();
+
+                        proccessRequisitionClient(clientSocket);
 
 
                   }
@@ -49,12 +57,27 @@ public class ServidorUm implements Runnable{ // Any class that implements the ru
             
 
 
-
+      
 
       
      
       }
-      @Override
+      private static void proccessRequisitionClient(Socket clientSocket) throws IOException {
+            try(InputStream input = clientSocket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input))){
+            
+                  // Ler os numeros recebidos do cliente
+                  int firstNumberR = Integer.parseInt(reader.readLine());
+                  int secondNumberR = Integer.parseInt(reader.readLine());
+
+                  System.out.println("Received numbers from client: " + firstNumberR + ", " + secondNumberR);
+
+
+            }
+
+            
+      }
+      
       public void run(){
             //run eh chamada quando a thread for executada
             System.out.println();
