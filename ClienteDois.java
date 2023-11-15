f2fba1b26b8030459ea54db7a2706d0e006345cb
Gabrielle Muniz <108156984+gabimusants@users.noreply.github.com>
2023-11-14 23:55:11 -0300

    alteracao conexao

--- ClienteDois.java
+++ ClienteDois.java
@@ -8,40 +8,51 @@ import java.util.Random;
 
 public class ClienteDois implements Runnable {
 
-	private PrintStream output;
+      private PrintStream out;
 
-	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
-		Socket socket = new Socket("localhost", 4440);
-		PrintStream output = new PrintStream(socket.getOutputStream());
+      public static void main(String[] arg) throws UnknownHostException, IOException, InterruptedException {
+            Socket sock = new Socket("localhost", 4000);
+            PrintStream output = new PrintStream(sock.getOutputStream());
+            System.out.println("Output Stream: " + output);
 
-		while (true) {
-			ClienteDois thread = new ClienteDois(output);
-			Thread t1 = new Thread(thread);
-			t1.start();
+            while (true) {
+                  // cria uma thread
+                  ClienteDois threadClient = new ClienteDois(output);
+                  Thread t = new Thread(threadClient);
+                  t.start();
 
-			Random random = new Random();
-			int minRange = 50;
-			int maxRange = 200;
-			int randomNumber = random.nextInt(maxRange - minRange + 1) + minRange;
+                  Random random = new Random();
+                  int minRange = 50;
+                  int maxRange = 200;
+                  int randomNumber = random.nextInt(maxRange - minRange + 1) + minRange;
 
-			Thread.sleep(randomNumber);
-		}
-	}
+                  t.sleep(randomNumber);
+            }
+      }
 
-	public ClienteDois(PrintStream output) {
-		this.output = output;
-	}
+      // preciso inicializar o constructor para poder enviar os dados ao servidor
+      public ClienteDois(PrintStream out) {
+            this.out = out;
+      }
 
-	public void run() {
+      public void run() {
+            // eh nessa funcao run onde ocorrera o processamento da thread
+            // Dado que a restricao eh:
+            // devem ser sorteados dois números entre 2 e 1.000.000, que
+            // serão enviados a um Servidor de Aplicação (por meio de um Balanceador de
+            // carga) para
+            // verificação do MDC (Máximo Divisor Comum) entre esses 2 números.
+            // static int sleepDuration = (int) (Math.random() * (200 - 50 + 1) +
+            // 50);//calcula os numeros entre 50 e 200, inclusive add 50 pra deslocar todo o
+            // intervalo pra que comece em 50
+            // thread tem que dormir por um tempo especificado entre 50 e 200 ms
+            Random rndOne = new Random();
+            Random rndTwo = new Random();
+            int randomOne = rndOne.nextInt(999999) + 2;
+            int randomTwo = rndTwo.nextInt(999999) + 2;
 
-		Random rndOne = new Random();
-		Random rndTwo = new Random();
-		int randomOne = rndOne.nextInt(999999) + 2;
-		int randomTwo = rndTwo.nextInt(999999) + 2;
-
-		// envia os numeros para o servidor
-		output.println(randomOne);
-		output.println(randomTwo);
-
-	}
+            // envia os numeros para o servidor
+            out.println(randomOne);
+            out.println(randomTwo);
+      }
 }
--- ServidorDois.java
+++ ServidorDois.java
@@ -9,154 +9,149 @@ import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Semaphore;
 
-public class ServidorDois implements Runnable {
-
+public class ServidorDois implements Runnable{
+	
 	private String strNumUm;
 	private String strNumDois;
-	private BufferedWriter writer;
-
+	//private String resp;
+  private BufferedWriter writer;
+    
 //    static int permits; 
-	Semaphore semaphore;
-
-	static int countWrite = 0;
-	static int countRead = 0;
-
+    Semaphore semaphore;
+    
+    static int countWrite = 0;
+    static int countRead = 0;
+    
 	public static void main(String[] args) throws IOException, InterruptedException {
-
-		ServerSocket serverSocket = new ServerSocket(4001);
-
+		
+		ServerSocket serverSocket = new ServerSocket(4000);
+		System.out.println("ServidorDois");
 		Semaphore semaphore = new Semaphore(1);
-
-		int countRead = 0;
-
+		
 		FileWriter file = new FileWriter("fileServidorUm.txt", true);
-		BufferedWriter writer = new BufferedWriter(file);
-
+	  BufferedWriter writer = new BufferedWriter(file);
+	    
 		String mensagem = "";
 		String strNumUm = "";
-		String strNumDois = "";
-
+		String strNumDois = "";	
+		
 		while (true) {
-
+			
 			Socket accept = serverSocket.accept();
-
+		
 			InputStreamReader inputReader = new InputStreamReader(accept.getInputStream());
 			BufferedReader reader = new BufferedReader(inputReader);
-
+			
 			while ((mensagem = reader.readLine()) != null) {
-
+				
 				if (mensagem != null) {
-
-					ServidorDois thread = new ServidorDois(strNumUm, strNumDois, writer, semaphore);
+					
+					ServidorDois thread = new ServidorDois(strNumUm, strNumDois, writer, semaphore);						
 					Thread t1 = new Thread(thread);
 					t1.start();
-					t1.join();
-
+					t1.join();		
 				}
-			}
+			}		
 		}
 	}
-
-	public void connection(int port, String text) throws UnknownHostException, IOException {
-
+	    	
+	public void connection(int port, String resp) throws UnknownHostException, IOException {
+		
 		Socket accept = new Socket("localhost", port);
-
-		if (text != null) {
+		
+		if (resp != null) {
 			PrintStream output = new PrintStream(accept.getOutputStream());
-			output.println(text);
-			output.println("4001");
+			output.println(resp);
+			output.println("4000");
 		}
-
 		accept.close();
-
-	}
-
-	public ServidorDois(String strNumUm, String strNumDois, BufferedWriter writer, Semaphore semaphore) {
+	}	
+	
+	public ServidorDois (String strNumUm, String strNumDois, BufferedWriter writer, Semaphore semaphore) {
 		this.strNumUm = strNumUm;
 		this.strNumDois = strNumDois;
 		this.writer = writer;
 		this.semaphore = semaphore;
-
+		
 	}
-
+	
 	public void run() {
-
+		
 		try {
-			if (isNumber(strNumUm) && isNumber(strNumDois)) {
-				int numUmInicio = Integer.parseInt(strNumUm);
-				int numUm = Integer.parseInt(strNumUm);
-				int numDois = Integer.parseInt(strNumDois);
-				String resp = "";
+			
+				if (isNumber(strNumUm) && isNumber(strNumDois)) {
+					int numUmInicio = Integer.parseInt(strNumUm);
+					int numUm = Integer.parseInt(strNumUm);
+					int numDois = Integer.parseInt(strNumDois);
+					
+					String resp = "";
+					int temp;
+					while (numDois != 0) {
+							temp = numDois;
+							numDois = numUm % numDois;
+							numUm = temp;
+					}
+					resp = "O MDC entre " + numUmInicio + "e " + numDois + "é: " + numUm;
+					countWrite++;
+					
+				 try {
+					 
+					 		semaphore.acquire();
+			          
+							writer.append(resp);
+							writer.newLine();
+							writer.flush();
 
-				int temp;
-				while (numDois != 0) {
-		            temp = numDois;
-		            numDois = numUm % numDois;
-		            numUm = temp;
-				}
-				resp = "O MDC entre " + numUmInicio + "e " + numDois + "é: " + numUm;
-				countWrite++;
+							connection(4000, resp); // Conexão com o Load Balance para envio da resposta.
 
-				try {
-
-					semaphore.acquire();
-					writer.append(resp);
-					writer.newLine();
-					writer.flush();
-
-					connection(5556, resp); // Conexão com o Load Balance para envio da resposta.
-					semaphore.release();
-
-				} catch (IOException e) {
-					System.out.println("Ocorreu um erro ao gravar o arquivo: " + e.getMessage());
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-			} else if (strNumUm.equals("Read")) {
-
-				countRead++;
-				LocalTime currentTime = LocalTime.now();
-				System.out.println(currentTime.getHour() + ":" + currentTime.getMinute() + ":" + currentTime.getSecond()
-						+ " - Servidor 1: " + countRead + " requisições de leitura e " + countWrite
-						+ " requisições de escrita.");
-
+							semaphore.release();
+			            
+			    } catch (IOException e) {
+			        System.out.println("Ocorreu um erro ao gravar o arquivo: " + e.getMessage());
+			    } catch (InterruptedException e) {
+							e.printStackTrace();
+					}
+				
 			} else {
+					try {
+					
+						semaphore.acquire();
 
-				try {
-
-					semaphore.acquire();
-
-					writer.append(strNumUm);
-					writer.append(strNumDois);
-					writer.newLine();
-					writer.flush();
-
-					semaphore.release();
-
-				} catch (IOException e) {
-					System.out.println("Ocorreu um erro ao atualizar o arquivo: " + e.getMessage());
-				} catch (InterruptedException e) {
-
-					e.printStackTrace();
+						writer.append(strNumUm);
+						writer.append(strNumDois);
+						writer.newLine();
+						writer.flush();
+									
+						semaphore.release();
+   
+		      } catch (IOException e) {
+		            System.out.println("Ocorreu um erro ao atualizar o arquivo: " + e.getMessage());
+		      } catch (InterruptedException e) {
+							e.printStackTrace();
 				}
-			}
 
-		} catch (NumberFormatException e) {
+			}
+			
+		} catch (NumberFormatException e){
 			System.out.println("Número inválido!");
 		}
-	}
 
-	public static boolean isNumber(String str) {
-		try {
-			Double.parseDouble(str);
-			return true;
-		} catch (NumberFormatException e) {
-			return false;
-		}
 	}
+	
+	public static boolean isNumber(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+	
+
 }
